3c9e391f2f8a76d6f8a62e16939f89393e7b5340
Xyvyrianeth <strobinsson@gmail.com>
2025-07-15 16:50:59 -0500

    1.2.0.2: fully realized the resize bar, now just need to copy it to align.js

--- scripts/tier.js
+++ scripts/tier.js
@@ -3,12 +3,9 @@
 
 	if (!/^https?:\/\/tiermaker\.com\/create\/.+$/.test(document.URL)) return;
 
-	const $ID    = (target) => { return document.getElementById(target) };
-	const $TAG   = (target) => { return document.getElementsByTagName(target) };
-	const $CLASS = (target) => { return document.getElementsByClassName(target) };
-
-	// Stylize the Necessary Shit
-	const defaultRatio = "55vw";
+	const $ID    = target => document.getElementById(target);
+	const $TAG   = target => document.getElementsByTagName(target);
+	const $CLASS = target => document.getElementsByClassName(target);
 	const bannerHeight = "170px";
 
 	const header = $ID("header");
@@ -17,26 +14,28 @@
 
 	const tier = $ID("tier-wrap");
 	tier.addClass("do-not-delete");
-	tier.css("position",  "fixed")
-		.css("top",       "165px")
-		.css("left",      "5px")
-		.css("width",     defaultRatio)
-		.css("height",    `calc(100% - ${bannerHeight})`)
-        .css("margin",    "0px")
-		.css("maxWidth",  "none")
-		.css("overflowY", "auto")
-		.css("overflowX", "clip");
+	tier.css("position",        "fixed")
+		.css("top",             "165px")
+		.css("left",            "5px")
+		.css("height",          `calc(100% - ${bannerHeight})`)
+        .css("margin",          "0px")
+		.css("maxWidth",        "none")
+		.css("scrollbar-width", "none")
+		.css("overflowY",       "auto")
+		.css("overflowX",       "clip");
 
 	const list = $ID("char-tier-outer-container-scroll");
 	list.addClass("do-not-delete");
-	list.css("position",  "fixed")
-		.css("margin",    "0px 0px 0px 0px")
-		.css("top",       "165px")
-		.css("right",     "5px")
-		.css("width",     `calc(100% - ${defaultRatio} - 20px)`)
-		.css("height",    `calc(100% - ${bannerHeight})`)
-		.css("overflowY", "auto")
-		.css("overflowX", "clip");
+	list.css("position",        "fixed")
+		.css("margin",          "0px 0px 0px 0px")
+		.css("top",             "165px")
+		.css("right",           "5px")
+		.css("height",          `calc(100% - ${bannerHeight})`)
+		.css("scrollbar-width", "none")
+		.css("overflowY",       "auto")
+		.css("overflowX",       "clip");
+
+	console.log(list);
 
 	const resizer = document.createElement("div");
 	resizer.id = "resizer";
@@ -46,7 +45,6 @@
 		   .css("zIndex",       "2")
 		   .css("width",        "5px")
 		   .css("height",       `calc(100vh - 170px)`)
-		   .css("left",         `calc(${defaultRatio} + 7.5px)`)
 		   .css("top",          "165px")
 		   .css("cursor",       "col-resize")
 		   .css("background",   "rgba(255, 255, 255, 0.4)")
@@ -54,30 +52,33 @@
 		   .css("padding",      "0px")
 		   .css("boxSizing",    "border-box")
 		   .css("borderRadius", "5px")
-           .css("-webkit-touch-callout", "none")
+           .css("-webkit-touch-callout", "none") // make your cursor not select shit while resizing
            .css("-webkit-user-select",   "none")
            .css("-khtml-user-select",    "none")
            .css("-moz-user-select",      "none")
            .css("-ms-user-select",       "none")
            .css("user-select",           "none");
-	resizer.addEventListener("mousedown", event => {
+	resizer.addEventListener("mousedown", () => {
 		document.addEventListener("mousemove", resize, false);
 		document.addEventListener("mouseup", () => {
 			document.removeEventListener("mousemove", resize, false);
 		}, false);
 	});
-	const resize = cursor => {
-        var position = (cursor.x / window.innerWidth * 10000 | 0) / 100;
-        if (position < 25) position = 25;
-        if (position > 80) position = 80;
+	const resize = (x, init) => {
+		var characterWidth = init ? 240 : $CLASS("character")[0].clientWidth;
+		var newListWidth = Math[init ? "floor" : "round"]((window.innerWidth - (x.x + 10)) / characterWidth) * characterWidth;
+		// var newListWidth = window.innerWidth - (x.x + 10);
+			newListWidth = newListWidth < characterWidth ? characterWidth : newListWidth;
+		var newRatio = newListWidth / window.innerWidth * 100;
+		console.log(x.x, characterWidth, newListWidth, newRatio);
 
-		const tierWidth = `calc(${position}vw - 10px)`;
-		const listWidth = `calc(100% - ${position}vw - 10px)`;
-		const rPosition = `calc(${position}vw - 2.5px)`;
-		tier.css("width", tierWidth);
-		list.css("width", listWidth);
-		resizer.css("left", rPosition);
+		list.css("width",   `calc(${newListWidth}px)`);
+		tier.css("width",   `calc(100% - ${newRatio}vw - 19px)`);
+		resizer.css("left", `calc(100% - ${newRatio}vw - 12px)`);
 	}
+	window.addEventListener("resize", () => resize({ x: window.innerWidth - $ID("char-tier-outer-container-scroll").getBoundingClientRect() }))
+	// document.addEventListener("load",   () => resize({x: window.innerWidth / 2}));
+	resize({ x: window.innerWidth / 2 }, true);
 
 	const saveButton = $ID("preview").parent();
 	saveButton.addClass("do-not-delete");
@@ -86,6 +87,7 @@
 			  .css("top",      "115px")
 			  .css("left",     "5px");
 	saveButton.click(() => { tier.css("height", "auto"); });
+	saveButton.childs()[0].text("Save");
 
 	const buttons = $ID("buttons");
 	buttons.addClass("do-not-delete");
@@ -97,9 +99,9 @@
 	buttons.childs()[0].childs()[0].remove();
 
 	const resetButton = buttons.childs()[0].childs()[0];
-	resetButton.css("backgroundColor", "red")
-			   .css("color",           "white")
-			   .css("fontSize",        "18px");
+	resetButton.css("background", "#dd0000")
+			   .css("color",      "#ffffff")
+			   .css("fontSize",   "18px");
 
 	const title = $TAG("h1")[0];
 	title.css("position", "fixed")
@@ -109,28 +111,24 @@
 		 .css("width", "100%")
 		 .css("height", "50px")
 		 .css("textAlign", "center");
-		//  .css("backgroundColor", "white");
     const tierlistTitle = document.getElementsByTagName("title")[0].text;
 	title.text(tierlistTitle.substring(9, tierlistTitle.length - 12));
 
 	const overlay = $ID("overlay");
 	overlay.addClass("do-not-delete");
 	$ID("export-container").click(() => {
-		tier.css("height", "calc(100% - 170px)")
-			 .css("width",  defaultRatio);
+		tier.css("height", "calc(100% - 170px)");
 	});
 
 	const altButton = $CLASS("button-link alignment-chart-btn")[0].parent();
-	// altButton.id = "align-comm-button";
 	altButton.addClass("do-not-delete");
-	altButton.css("position",     "fixed")
-			 .css("width",        `100%`)
-			 .css("left",         "0px")
-			 .css("top",          "110px")
-			 .css("margin",       "0px");
+	altButton.css("position", "fixed")
+			 .css("width",    `100%`)
+			 .css("left",     "0px")
+			 .css("top",      "110px")
+			 .css("margin",   "0px");
 	altButton.childs().forEach(element => element.css("height", "32px"));
 
-	// Delete the Useless Shit
 	const mainContainer = $ID("main-container");
 	const toDelete = mainContainer.childs().filter(element => {
 		if (element.hasClass("do-not-delete")) return false;
@@ -138,4 +136,5 @@
 		return true;
 	});
 	toDelete.forEach(element => element.remove());
+
 })();
